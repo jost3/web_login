77b2055cf5f77473c818f3d236227b59d0974693
jost3 <1427593@senati.pe>
2023-09-14 11:06:18 -0500

    se agrego un procedimiento alamcenado de insertar con una condicion

--- bd/Query1.sql
+++ bd/Query1.sql
@@ -12,3 +12,27 @@ Clave varchar(80),
 Nombre varchar(80),
 apellido varchar(80)
 )
+
+CREATE PROC SP_Registrar(
+@Correo varchar(80),
+@Clave varchar(80),
+@Nombre varchar(80),
+@apellido varchar(80),
+@Registrado bit output,
+@Mensaje varchar(100) output
+)
+as
+begin
+
+	if(not exists(select * from USUARIO where Correo = @Correo))
+	begin
+		insert into USUARIO (Correo,Clave,Nombre,apellido) values (@Correo,@Clave,@Nombre,@apellido)
+		set  @Registrado = 1
+		set  @Mensaje = 'usuario registrado'
+	end
+	else
+	begin
+		set @Registrado = 0
+		set	@Mensaje = 'Correo ya existente'
+	end
+end
